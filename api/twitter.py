3c89c4858d0d1bfd6995da1abb09e2308f3c2812
Jakub Powierza <jpowie01@gmail.com>
2019-01-20 01:08:58 +0100

    Fix attention and allow for neutral sentiment

--- api/twitter.py
+++ api/twitter.py
@@ -9,7 +9,7 @@ assert CONSUMER_KEY and CONSUMER_SECRET, 'Remember to set up your CONSUMER_KEY a
 
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = os.environ.get('ACCESS_TOKEN_SECRET')
-assert CONSUMER_KEY and CONSUMER_SECRET, 'Remember to set up your ACCESS_TOKEN and ACCESS_TOKEN_SECRET!'
+assert ACCESS_TOKEN and ACCESS_TOKEN_SECRET, 'Remember to set up your ACCESS_TOKEN and ACCESS_TOKEN_SECRET!'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
