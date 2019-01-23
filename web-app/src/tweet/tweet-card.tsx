ceb49c27bb26feb78fde038953ef2ecf11fa6c01
kolszewska <k.olszewskaaa@gmail.com>
2019-01-23 23:31:42 +0100

    Added coloring sentiment values based on sentiment, added photo, nick, fullname and created to the tweet card

--- web-app/src/tweet/tweet-card.tsx
+++ web-app/src/tweet/tweet-card.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import { Panel } from 'react-bootstrap';
+import {Panel} from 'react-bootstrap';
 import './tweet-card.css';
-import { WordAttentionPair } from './word-attention-pair';
+import {WordAttentionPair} from './word-attention-pair';
 
 export enum Sentiments {
     POSITIVE = "POSITIVE",
@@ -10,42 +10,41 @@ export enum Sentiments {
 }
 
 export class TweetCard extends React.Component<any, any> {
-    public state: { textWithAttentions: WordAttentionPair[], textSentiment: Sentiments };
+    public state: {
+        created: string, fullname: string, nickname: string, photoUrl: string,
+        textWithAttentions: WordAttentionPair[], textSentiment: Sentiments
+    };
 
-    constructor(props: { text: string, attentions: number[], sentiment: Sentiments }, context: any) {
+    constructor(props: {
+        created: string, fullname: string, nickname: string, photoUrl: string,
+        text: string, attentions: number[], sentiment: Sentiments
+    }, context: any) {
         super(props, context);
 
-        const preparedText: WordAttentionPair[] = this.prepareWordAttenitonPairs(props.text, props.attentions);
+        const preparedText: WordAttentionPair[] = this.prepareWordAttentionPairs(props.sentiment, props.text, props.attentions);
 
         this.state = {
+            created: props.created,
+            fullname: props.fullname,
+            nickname: props.nickname,
+            photoUrl: props.photoUrl,
             textSentiment: props.sentiment,
-            textWithAttentions: preparedText
+            textWithAttentions: preparedText,
         }; // intentional
     }
 
     public render() {
-        let tweetCardStyle: string = "";
-        switch(this.state.textSentiment) {
-            case Sentiments.POSITIVE: 
-                tweetCardStyle = "success"
-                break;
-            case Sentiments.NEUTRAL:
-                tweetCardStyle = "info"
-                break;
-            case Sentiments.NEGATIVE:
-                tweetCardStyle = "danger"
-                break;
-        }
 
         return (
             <div className='TweetCard'>
-                <Panel bsStyle={tweetCardStyle}>
-                    <Panel.Heading>
-                        <Panel.Title componentClass="h3">
-                            {this.state.textSentiment}
-                    </Panel.Title>
-                    </Panel.Heading>
+                <Panel>
                     <Panel.Body>
+                        <div className='TweetUserInfo'>
+                            <img src={this.state.photoUrl}/>
+                            <span className='UserFullName'>{this.state.fullname}</span>
+                            <span className='UserFullNick'>{'@' + this.state.nickname}</span>
+                            <span className='TweetCreated'>{this.state.created}</span>
+                        </div>
                         <span>
                             {this.state.textWithAttentions.map((pair, i) => pair.render())}
                         </span>
@@ -55,12 +54,16 @@ export class TweetCard extends React.Component<any, any> {
         );
     }
 
-    private prepareWordAttenitonPairs(text: string, attentions: number[]): WordAttentionPair[] {
+    private prepareWordAttentionPairs(sentiment: Sentiments, text: string, attentions: number[]): WordAttentionPair[] {
         const words: string[] = text.split(" ");
         const wordAttentionPairs: WordAttentionPair[] = [];
 
         words.forEach((value: string, index: number, array: string[]) => {
-            wordAttentionPairs.push(new WordAttentionPair({word: value, attention: attentions[index]}, this.context));
+            wordAttentionPairs.push(new WordAttentionPair({
+                attention: attentions[index],
+                sentiment,
+                word: value,
+            }, this.context));
         });
 
         return wordAttentionPairs;
--- web-app/src/tweet/word-attention-pair.tsx
+++ web-app/src/tweet/word-attention-pair.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
+import {Sentiments} from "./tweet-card";
+
+
+function GetColour(sentiment: Sentiments) {
+    switch (sentiment) {
+        case Sentiments.POSITIVE:
+            return '0,255,0';
+        case Sentiments.NEUTRAL:
+            return '0,0,255';
+        case Sentiments.NEGATIVE:
+            return '255,0,0';
+        default:
+            return '255,255,0'
+    }
+}
 
 export class WordAttentionPair extends React.Component<any, any> {
-    public state: {word: string, attention: number};
+    public state: { sentiment: Sentiments, word: string, attention: number };
 
-    constructor(props: { word: string, attention: number}, context: any) {
+    constructor(props: { sentiment: Sentiments, word: string, attention: number }, context: any) {
         super(props, context);
 
         this.state = props;
@@ -11,12 +26,12 @@ export class WordAttentionPair extends React.Component<any, any> {
 
     public render() {
         const textStyle = {
-            backgroundColor: 'rgba(255,255,0,' + this.state.attention + ')',
+            backgroundColor: 'rgba(' + GetColour(this.state.sentiment) + ',' + this.state.attention + ')',
             display: 'inline-block'
         };
         const spanStyle = {
             display: 'inline-block'
-        }
+        };
         return (
             <span style={spanStyle}>
                 &nbsp;<p style={textStyle}>{this.state.word}</p>
