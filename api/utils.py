2b960f9ef86e48dbe3c8539921d17f3f110e1017
Jakub Powierza <jpowie01@gmail.com>
2019-01-24 20:56:27 +0100

    Fix critical issue with Tokenizer (#11)

--- api/utils.py
+++ api/utils.py
@@ -17,7 +17,7 @@ def clean_text(text, remove_words=True):
     text = ' '.join(re.sub(r"(@[A-Za-z0-9]+( tweeted:)?)|([^0-9A-Za-z \t])|(https?\S*)|(\w+:\/\/\S+)"
                            , "UNK", text).split())
     tokens = nltk.word_tokenize(text)
-    tokens = [word if word in stopwords.words('english') else "UNK" for word in tokens]
+    tokens = [word if word not in stopwords.words('english') else "UNK" for word in tokens]
 
     stems = [stemmer.stem(item) for item in tokens]
     return ' '.join(stems)
